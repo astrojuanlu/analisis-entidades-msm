362ecbc9a45d588c392f0761d22946976b399d91
Juan Luis Cano Rodríguez <hello@juanlu.space>
2022-11-30 13:21:11 +0100

    Smarter data merging

--- merge_data.py
+++ merge_data.py
@@ -1,64 +1,48 @@
+import os
+
 import polars as pl
 
-import recordlinkage
 from recordlinkage.preprocessing import clean
 
 df_catalog = pl.read_parquet("api.parquet")
 df_benefits = pl.read_parquet("benefits.parquet")
 
-catalog_cols = df_catalog.columns
+# If the file exists, just use it
+# This allows operators to edit it manually
+if not os.path.isfile("entity_pairs.csv"):
+    df_benefits.select(
+        [
+            pl.col("name_in_benefits"),
+            pl.from_pandas(
+                clean(
+                    df_benefits["name_in_benefits"].to_pandas(), strip_accents="ascii"
+                )
+            ).alias("entity_name_clean"),
+        ]
+    ).join(
+        df_catalog.select(
+            [
+                pl.col("name").alias("name_in_catalog"),
+                pl.from_pandas(
+                    clean(df_catalog["name"].to_pandas(), strip_accents="ascii")
+                ).alias("entity_name_clean"),
+            ]
+        ),
+        on="entity_name_clean",
+        how="left",
+    ).write_csv(
+        "entity_pairs.csv"
+    )
 
-df_catalog = df_catalog.with_columns(
-    [
-        pl.from_pandas(
-            clean(df_catalog["name"].to_pandas(), strip_accents="ascii")
-        ).alias("entity_name_clean")
-    ]
-)
-df_benefits = df_benefits.with_columns(
-    [
-        pl.from_pandas(
-            clean(df_benefits["name_in_benefits"].to_pandas(), strip_accents="ascii")
-        ).alias("entity_name_clean")
-    ]
-)
+entity_pairs = pl.read_csv("entity_pairs.csv")
 
-indexer = recordlinkage.Index()
-# The DataFrames are small, so we can afford creating a Full indexer
-indexer.full()
-pairs_pd = indexer.index(df_catalog.to_pandas(), df_benefits.to_pandas())
+df = df_catalog.join(
+    entity_pairs.select(pl.all().exclude("entity_name_clean")),
+    left_on="name",
+    right_on="name_in_catalog",
+    how="left",
+).join(df_benefits, on="name_in_benefits", how="left")
 
-comparer = recordlinkage.Compare()
-comparer.string("entity_name_clean", "entity_name_clean")
-features_pd = comparer.compute(
-    pairs_pd, df_catalog.to_pandas(), df_benefits.to_pandas()
-)
-
-features = pl.from_pandas(features_pd.reset_index()).select(
-    [
-        pl.col("level_0").alias("catalog_idx"),
-        pl.col("level_1").alias("benefits_idx"),
-        pl.col("0").alias("score"),
-    ]
-)
-
-matches = (
-    features.filter(pl.col("score") > 0.5)  # Only keep likely matches
-    .groupby("catalog_idx")
-    .agg([pl.col("benefits_idx").sort_by("score", reverse=True).first()])
-    .sort("catalog_idx")
-)
-
-df_catalog = df_catalog.with_column(pl.arange(0, len(df_catalog)).alias("catalog_idx"))
-df_benefits = df_benefits.with_column(
-    pl.arange(0, len(df_benefits)).alias("benefits_idx")
-)
-
-df = (
-    df_catalog.drop(["entity_name_clean"])
-    .join(matches, on="catalog_idx", how="left")
-    .join(df_benefits.drop(["entity_name_clean"]), on="benefits_idx", how="left")
-)
 df.select(pl.all().exclude(pl.List(pl.Utf8))).sort("name").write_csv(
     "entities_full.csv"
 )
