47c80241e22fdfdaf7a528b6fccfaad414c8a232
Juan Luis Cano Rodríguez <hello@juanlu.space>
2022-11-30 12:23:07 +0100

    Include categories list in cleaned API

--- clean_api.py
+++ clean_api.py
@@ -12,6 +12,7 @@ def trim_record(record):
         "address",
         "short_description",
         "description",
+        "categories",
         "email",
         "hidden",
         "inactive",
@@ -27,9 +28,28 @@ def main():
         data = json.load(fh)
         df = pl.from_records([trim_record(record) for record in data])
 
+    with open("categories.json") as fh:
+        data = json.load(fh)
+        df_categories = pl.from_records(data)
+
     df = df.filter(~pl.col("hidden") & ~pl.col("inactive")).select(
         pl.all().exclude(["hidden", "inactive"])
     )
+    category_pairings = (
+        df.explode("categories")
+        .select([pl.col("id"), pl.col("categories")])
+        .join(
+            df_categories.select(
+                [pl.col("id").alias("category_id"), pl.col("name").alias("category")]
+            ),
+            left_on="categories",
+            right_on="category_id",
+            how="left",
+        )
+        .groupby("id")
+        .agg(pl.col("category").list().alias("categories"))
+    )
+    df = df.select(pl.all().exclude("categories")).join(category_pairings, on="id")
 
     df.write_parquet("api.parquet")
 
