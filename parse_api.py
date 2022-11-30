366d22a4fb61e2b08619573e18c3b4f569d4db2c
Juan Luis Cano Rodríguez <hello@juanlu.space>
2022-11-30 12:04:22 +0100

    Parse categories from the API as well

--- parse_api.py
+++ parse_api.py
@@ -6,12 +6,11 @@ import httpx
 logger = logging.getLogger(__name__)
 
 BASE_URL = "https://app.mercadosocial.net"
-API_URL = BASE_URL + "/api/v1/entities/"
+ENTITIES_URL = BASE_URL + "/api/v1/entities/"
+CATEGORIES_URL = BASE_URL + "/api/v1/categories/"
 
 
-def main():
-    data = []
-    url = API_URL
+def scrape_api(url, key, base_url=BASE_URL):
     while True:
         logger.info("Reading URL %s", url)
 
@@ -20,15 +19,24 @@ def main():
 
         resp_json = resp.json()
 
-        data.extend(resp_json["entities"])
+        yield from resp_json[key]
 
         if resp_json["meta"]["next"]:
-            url = BASE_URL + resp_json["meta"]["next"]
+            url = base_url + resp_json["meta"]["next"]
         else:
             break
 
+
+def main():
+    entities = list(scrape_api(ENTITIES_URL, "entities"))
+
     with open("api.json", "w") as fh:
-        json.dump(data, fh)
+        json.dump(entities, fh)
+
+    categories = list(scrape_api(CATEGORIES_URL, "categories"))
+
+    with open("categories.json", "w") as fh:
+        json.dump(categories, fh)
 
 
 if __name__ == "__main__":
